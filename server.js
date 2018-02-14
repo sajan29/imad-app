68f212b0ede80abf626cd3edf1c3d5674c64ae1f
Sajan Kumar Shaw <sajanshaw8981@gmail.com>
2018-02-14 22:35:27 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,11 +5,71 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var articleOne={
+    title:'article-one|sajan',
+    heading:'article-one',
+    date:'13 Feb 2018',
+    content:`
+        <p>
+          This is the content of the Article-one.This is the content of the Article-one.This is the content of the Article-one.
+          This is the content of the Article-one.This is the content of the Article-one.This is the content of the Article-one.
+          This is the content of the Article-one.This is the content of the Article-one.This is the content of the Article-one.
+       </p>
+        <p>
+          This is the content of the Article-one.This is the content of the Article-one.This is the content of the Article-one.
+          This is the content of the Article-one.This is the content of the Article-one.This is the content of the Article-one.
+          This is the content of the Article-one.This is the content of the Article-one.This is the content of the Article-one.
+       </p>
+        <p>
+          This is the content of the Article-one.This is the content of the Article-one.This is the content of the Article-one.
+          This is the content of the Article-one.This is the content of the Article-one.This is the content of the Article-one.
+          This is the content of the Article-one.This is the content of the Article-one.This is the content of the Article-one.
+       </p>
+        <p>
+          This is the content of the Article-one.This is the content of the Article-one.This is the content of the Article-one.
+          This is the content of the Article-one.This is the content of the Article-one.This is the content of the Article-one.
+          This is the content of the Article-one.This is the content of the Article-one.This is the content of the Article-one.
+       </p>`
+};
+function createTemplate(data){
+var title=data.title;
+var heading=data.heading;
+var date=data.date;
+var content=data.content;
+var htmlTemplate = `
+<html>
+ <head>
+  $(title)
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="/ui/style.css" rel="stylesheet" />
+</head>
+<body>
+    <div class="container">
+    <div>
+        <a href="/">Home</a>
+    </div>
+    <hr>
+    <div>
+        $(heading)
+    </div>
+    <div>
+      $(date)
+    </div>
+    <div>
+      $(content)
+    </div>
+    </div>
+</body>
+</html>
+`;
+return htmlTemplate;
+}
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 app.get('/article-one',function(req,res){
- res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+ res.send(createTemplate(articleOne));
 });
 app.get('/article-two',function(req,res){
  res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
