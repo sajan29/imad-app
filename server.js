78ca368f1d21446179b72cf7c6504dc1fcfd88be
Sajan Kumar Shaw <sajanshaw8981@gmail.com>
2018-03-04 17:57:46 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -39,9 +39,9 @@ var content=data.content;
 var htmlTemplate =`
 <html>
  <head>
- 
+ <title>
   ${title}
- 
+ </title>
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link href="/ui/style.css" rel="stylesheet" />
 </head>
