549fe59938d197ba77d74b67f090dabbffd9ee23
Sajan Kumar Shaw <sajanshaw8981@gmail.com>
2018-03-04 20:57:43 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -103,9 +103,7 @@ app.get('/ui/style.css', function (req, res) {
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
-app.get('/ui/sajan.png', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'sajan.png'));
-});
+
 
 
 // Do not change port, otherwise your app won't run on IMAD servers
