e3e1b8860239748f251a120ff5d116cd610d8571
Sajan Kumar Shaw <sajanshaw8981@gmail.com>
2018-03-04 18:58:55 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,3 +2,8 @@ console.log('Loaded!');
 //changing the main text on index.html
 var element=document.getElementById('main-text');
 element.innerHTML='New Value';
+//changing the img position
+var img=document.getElementById('logo');
+img.onclick(function(){
+    img.style.marginLeft='100px';
+});
