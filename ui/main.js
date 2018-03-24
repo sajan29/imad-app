790d8a87ee8045915d1271135f636bfe47efdffc
Sajan Kumar Shaw <sajanshaw8981@gmail.com>
2018-03-24 19:00:33 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,20 +1,4 @@
-/*console.log('Loaded!');
-//changing the main text on index.html
-var element=document.getElementById('main-text');
-element.innerHTML='New Value';
-//changing the img position
-var img=document.getElementById('logo');
-var margin=0;
-function moveRight()
-{
-     margin=margin+10;
-    img.style.marginLeft=margin+'px';
-  
-}
-img.onclick=function(){
-    var interval=setInterval(moveRight,100);
-   
-};*/
+
 var button=document.getElementById('counter');
 button.onclick=function()
 {
