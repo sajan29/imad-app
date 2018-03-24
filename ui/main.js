218ecb6ee837171f5bdc232c9454bc9e9589e5b0
Sajan Kumar Shaw <sajanshaw8981@gmail.com>
2018-03-24 19:21:35 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -18,6 +18,6 @@ button.onclick=function()
     };
     
     //render the variable in the correct span
-    request.open("GET", "http://sajanshaw387.imad.hasura-app.io", true);
+    request.open("GET", "http://sajanshaw387.imad.hasura-app.io/counter", true);
     request.send(null);   
 };
