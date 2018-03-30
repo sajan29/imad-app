0503cb94d2a555adf409fe97971616ea05b0978c
Sajan Kumar Shaw <sajanshaw8981@gmail.com>
2018-03-30 18:06:49 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -120,20 +120,7 @@ app.listen(port, function () {
 });
 
 //server request
-var nameInput=document.getElementById("name");
-var input=nameInput.value;
-var submit=document.getElementById("submit_btn");
-var output=submit.onclick(function(){
-    var names=['name1','name2','name3','name4'];
-    var list="";
-    for(var i=0;i<names.length;i++)
-    {
-        list+='<li>'+names[i]+'</li>';
-    }
-    var ul=document.getElementById('namelist');
-    ul.innerHTML=list;
-    
-});
+
 
 
 
