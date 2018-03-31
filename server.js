b2f3f609f1be99ea45962315951c57c75e5878ce
Sajan Kumar Shaw <sajanshaw8981@gmail.com>
2018-03-31 11:46:29 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -94,7 +94,7 @@ app.get('/articles/:articleName',function(req,res){
    // var articleName=req.params.articleName;
    // res.send(createTemplate(articles[articleName]));
    
-   pool.query("SELECT * FROM article WHERE title=$1",req.params.articleName,function(err,result){
+   pool.query("SELECT * FROM article WHERE title=$1",[req.params.articleName],function(err,result){
       
       if(err)
       {
