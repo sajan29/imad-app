292743e73465be39bd398a659389720b86588ff1
Sajan Kumar Shaw <sajanshaw8981@gmail.com>
2018-04-10 18:00:15 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,4 +1,4 @@
-
+//counter code
 var button=document.getElementById('counter');
 button.onclick=function()
 {
@@ -52,6 +52,44 @@ submit.onclick=function(){
     //render the variable in the correct span
     request.open("GET", "http://sajanshaw387.imad.hasura-app.io/submit-name?name="+input, true);
     request.send(null);   
+   
+    
+};
+//login by using username and password
+var submit=document.getElementById("submit_btn");
+submit.onclick=function(){
+    
+   
+    var request=new XMLHttpRequest();
+    //Capture the response and store it in a variable
+    request.onreadystatechange = function() {
+      if(request.readyState===XMLHttpRequest.DONE)
+      {
+        if(request.status===200)
+        {
+           
+           alert('user logged in');
+        }
+        else if(request.status===403)
+        {
+           
+           alert('username or password is incorrect');
+        }
+       else if (request.status===500)
+        {
+          
+           alert('Something is wrong');
+        }
+      }
+    };
+    var username=document.getElementById("username").value;
+    var password=document.getElementById("password").value;
+   
+    
+    //render the variable in the correct span
+    request.open("POST", "http://sajanshaw387.imad.hasura-app.io/login", true);
+    request.setRequestHeader('Content-Type','application/json');
+    request.send(JSON.stringify({username : username ,password : password}));   
    
     
 };
