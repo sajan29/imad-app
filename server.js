905e4e9b8028f3e5b8f380d5827fff96488e9249
Sajan Kumar Shaw <sajanshaw8981@gmail.com>
2018-04-10 18:15:23 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -196,7 +196,7 @@ app.post('/login',function(req,res){
 app.get('/check-login',function(req,res){
    if(req.session && req.session.auth &&req.session.auth.userId)
    {
-       res.send('User logged in with userId'+req.session.auth.userId.toString());
+       res.send('User logged in with userId '+req.session.auth.userId.toString());
    }
    else
     {
